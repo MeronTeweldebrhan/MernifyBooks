25274f84ffc5d415f7f7a8c1c5472f1f34bedbb5
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-12 00:01:53 -0400

    feat: add MongoDB connection using Mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // server/server.js
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB=require('./config/db')
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
