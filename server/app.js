842b8c24dd7413a4ddcbed1a37cab3de84ac1304
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-11 23:49:00 -0400

    chore: initial backend setup for MernifyBooks with Express and environment config

---
+++ server/app.js
@@ -0,0 +1,16 @@
+// server/app.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('MernifyBooks API is running...');
+});
+
+module.exports = app;
