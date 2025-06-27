dc452f2f122ed4e79c44ef8d5a0b4ee30d079285
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-26 23:54:44 -0400

    feat(auth): add user registration and login with JWT authentication

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const accountRoutes =require('./routes/accountRoutes')
 const transactionRoutes = require('./routes/transactionRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 
 
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/api/accounts', accountRoutes);
 app.use('/api/transactions', transactionRoutes);
+app.use('/api/auth', authRoutes);
 
 // Sample route
 app.get('/', (req, res) => {
